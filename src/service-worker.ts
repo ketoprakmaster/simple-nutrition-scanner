de72b5243fe23c5228db10e173c14566951a9626
ketoprakmaster <82126344+ketoprakmaster@users.noreply.github.com>
2026-02-08 09:13:53 +0700

    added: service worker, added cache for api calls and images

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -6,6 +6,8 @@
 import { build, files, version, prerendered, base } from '$service-worker';
 import { precacheAndRoute, cleanupOutdatedCaches, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute, NavigationRoute } from 'workbox-routing';
+import { StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
+import { ExpirationPlugin } from 'workbox-expiration';
 
 const self = globalThis.self as unknown as ServiceWorkerGlobalScope;
 
@@ -38,15 +40,56 @@ precacheAndRoute(manifest);
 cleanupOutdatedCaches();
 
 // SHELL fallback logic
-try {
-    registerRoute(new NavigationRoute(
-        createHandlerBoundToURL(SHELL)
+registerRoute(new NavigationRoute(
+    createHandlerBoundToURL(SHELL)
 ));
-} catch (error) {
-	console.error('Workbox NavigationRoute failed. Falling back to manual shell.', error);
-}
 
-self.addEventListener('install', () => self.skipWaiting());
+// Cache API calls
+registerRoute(
+	({ url }) => url.origin === 'https://world.openfoodfacts.org',
+	new StaleWhileRevalidate({
+		cacheName: 'food-api-cache',
+		plugins: [
+			new ExpirationPlugin({
+				maxEntries: 50,
+				maxAgeSeconds:  24 * 60 * 60, // 1 day
+			}),
+		],
+	})
+);
+
+// Cache Food Images
+registerRoute(
+	({ url }) => url.origin === 'https://images.openfoodfacts.org',
+	new CacheFirst({
+		cacheName: 'food-images-cache',
+		plugins: [
+			new ExpirationPlugin({
+				maxEntries: 50,
+				maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+			}),
+		],
+	})
+);
+
+// Cache Static Assets
+registerRoute(
+	({ url }) => url.origin === 'https://static.openfoodfacts.org',
+	new CacheFirst({
+		cacheName: 'off-static-assets',
+		plugins: [
+			new ExpirationPlugin({
+				maxEntries: 50,
+				maxAgeSeconds: 60 * 60 * 24 * 90, // 90 Days
+			}),
+		],
+	})
+);
+
+self.addEventListener('install', () => {
+    self.skipWaiting()
+});
+
 self.addEventListener('activate', (event) => {
 	event.waitUntil(self.clients.claim());
 });
